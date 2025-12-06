bc72b8740ad469eec39615a75fa9050ed1751951
kaeraeaa <129053421+kaeraeaa@users.noreply.github.com>
2025-12-06 22:27:57 +0200

    Add background

--- investiq/src/components/App/App.jsx
+++ investiq/src/components/App/App.jsx
@@ -4,12 +4,15 @@ import Header from '../Header/Header';
 import Container from '../Container/Container';
 import DashboardPage from '../../pages/DashboardPage/DashboardPage';
 
+import Background from '../Background/Background';
+
 
 export default function App() {
     return (
         <div className="app">
+             <Background />
             <Header />
-           
+          
             <Container>
                 <DashboardPage />
             </Container>
